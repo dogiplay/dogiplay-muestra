36a596a0441f1c4dfc4bf781a70eebd73c6aec02
Alfonso Lopez <alfonso.jlopezcuevas@gmail.com>
2024-05-06 17:46:21 -0700

    INTENTO X

--- src/componentes/Contacto.js
+++ src/componentes/Contacto.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { CardContacto } from '../ui-components'
 
@@ -9,15 +8,3 @@ export const Contacto = () => {
 </div>
   )
 }
-=======
-import React from 'react'
-import { CardContacto } from '../ui-components'
-
-export const Contacto = () => {
-  return (
-    <div class="contenedor-inicio" >
-    <div class="contenedorInicioDeportes"><CardContacto/></div>
-</div>
-  )
-}
->>>>>>> 03313ea2ab0aca5b84a1b231f11936c66040fe81
--- src/rutas/RutasPrincipal.js
+++ src/rutas/RutasPrincipal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import {Routes, Route, BrowserRouter, NavLink} from 'react-router-dom';
 import { NayaritBeisbolLiga1 } from '../NayaritBeisbolLiga1';
@@ -11,7 +10,6 @@ import { Logodogiplay, Rol } from '../ui-components';
 
 
 export const RutasPrincipal = () => {
-  // const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
   return (
     <BrowserRouter>
     <header className='header'>
@@ -38,76 +36,13 @@ export const RutasPrincipal = () => {
             <Route path='/Inicio' element={<Inicio/>}/>
             <Route path='/Contacto' element={<Contacto/>}/>
             <Route path='/nayaritbeisbolliga1' element={<NayaritBeisbolLiga1/>}/>
-            <Route path='/registro' element={<Rol/>}/>
             <Route path='/registropresidente' element={<RegistroPresidente/>}/>
             <Route path='/registromanager' element={<RegistroManager/>}/>
         </Routes>
       </section>
-      {/* <div>
-         <button onClick={() => toPDF()}>Download PDF</button>
-         <div ref={targetRef}>
-            Content to be generated to PDF
-         </div>
-      </div> */}
+      
     </BrowserRouter>
     
     
   )
 }
-=======
-import React from 'react'
-import {Routes, Route, BrowserRouter, NavLink} from 'react-router-dom';
-import { NayaritBeisbolLiga1 } from '../NayaritBeisbolLiga1';
-import { RegistroPresidente } from '../componentes/RegistroPresidente';
-import { RegistroManager } from '../componentes/RegistroManager';
-import { Inicio } from '../Inicio';
-import { Contacto } from '../componentes/Contacto';
-import { Logodogiplay, Rol } from '../ui-components';
-// import { usePDF } from 'react-to-pdf';
-
-
-export const RutasPrincipal = () => {
-  // const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
-  return (
-    <BrowserRouter>
-    <header className='header'>
-         
-      <nav>
-      <Logodogiplay className='logo'/>  
-      <ul>
-      <li>
-            <NavLink to="/"><strong>Inicio</strong></NavLink>
-          </li> 
-          <li>
-            <NavLink to="/contacto"><strong>Contacto</strong></NavLink>
-          </li>  
-          
-          </ul>
-     
-          <strong class='derecha'>Las ligas serias de México están en DOGIPLAY</strong>
-      </nav>
-    </header>
-   
-      <section className='content'>
-        <Routes>
-            <Route path='/' element={<Inicio/>}/>
-            <Route path='/Inicio' element={<Inicio/>}/>
-            <Route path='/Contacto' element={<Contacto/>}/>
-            <Route path='/nayaritbeisbolliga1' element={<NayaritBeisbolLiga1/>}/>
-            <Route path='/registro' element={<Rol/>}/>
-            <Route path='/registropresidente' element={<RegistroPresidente/>}/>
-            <Route path='/registromanager' element={<RegistroManager/>}/>
-        </Routes>
-      </section>
-      {/* <div>
-         <button onClick={() => toPDF()}>Download PDF</button>
-         <div ref={targetRef}>
-            Content to be generated to PDF
-         </div>
-      </div> */}
-    </BrowserRouter>
-    
-    
-  )
-}
->>>>>>> 03313ea2ab0aca5b84a1b231f11936c66040fe81
